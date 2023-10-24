21741dad6e89f9a69ae90ef7fe3db9d8d67daa54
33-66 <simon.nganga@student.moringaschool.com>
2023-10-24 12:18:58 +0300

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(books => books.forEach(book => renderBooks(book)))
 }
 
-function renderBooks(books) {
+function renderBooks(book) {
   const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+  const h2 = document.createElement('h2');
+  h2.innerHTML = book.name;
+  main.appendChild(h2);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
